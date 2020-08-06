579dd476ff52a2d52b52abca58efd7ec13e64674
Antony Baasandorj <antony.baasan@gmail.com>
2020-08-06 18:41:23 -0400

    add Ocr controller

---
+++ server/Controllers/Ocr.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace server.Controllers
+{
+    public class Ocr : Controller
+    {
+        // GET
+        public string Index()
+        {
+            return "hello world";
+        }
+    }
+}
